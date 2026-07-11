3c602e7aa3cdc784fede703205b364b1343077c2
Carlos Marcos <carlos@hotmail.com>
2026-07-11 09:15:34 -0500

    dom

--- maniupulacionDOM.js
+++ maniupulacionDOM.js
@@ -4,6 +4,6 @@
 
 // El navegador ofrece la interfaz llamada DOM para que JavaScript pueda comunicarse
 // con el documento HTML.
-// El DOM es la representación en objeto del documento HTML en
+// El DOM es la representación en forma de objetos del documento HTML que ecrea el navegador
 // la cual ese objeto tiene ya métodos nativos para poder manipular
 // el HTML o los nodos
