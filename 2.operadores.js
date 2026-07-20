c8bb9e11e558e42b3d3500a88e0f39c3a1cf221e
Carlos Marcos <carlos@kuali.ai>
2026-07-20 08:00:23 -0500

    feat: definicion de operadores

--- 2.operadores.js
+++ 2.operadores.js
@@ -1,1 +1,77 @@
-// Los operadores devuelven un valor, 
+// Operadores:
+/* Un operador es un simbolo o conjuntos de simbolos que realiza una operacion
+sobre uno o mas operandos y devuelve un valor (un tipo de dato)*/
+/* Ese resultado puede ser un número, un booleano, un string, etc*/
+
+// 1.- Operadores aritmeticos:
+/* Son operadores que realizacion operaciones matematicas entre uno o mas operandos
+que deben ser numeros*/
+
+// Unario: Operador que realiza una operacion sobre un solo operando
+// Binario: Operador que realiza una operacion sobre dos operandos
+// Ternario: Operador que realiza una operacion sobre tres operandos
+
+/*
+// Unarios:
+++
+--
+
+/* ejemplo
+
+let numero = 1;
+console.log(++numero) // 2
+console.log(--numero) // 1
+*/
+
+/*
+// Binarios:
++
+-
+*
+/
+%
+*/
+
+// 2.- Operadores de asignacion: (=)
+/* Su funcion es asignar un valor a una variable. Por ejemplo:
+
+let edad = 25;
+let numero = 10;
+
+numero = 20;
+
+let puntos = 10;
+
+puntos += 5;
+puntos = puntos + 5;
+
+*/
+
+
+// 3.- Operadores de Comparacion:
+/* Son operadores que comparan dos valores y siempre devuelve un dato booleano
+
+10<5 --- false
+10>5 --- true
+10==5 --- false
+10!=5 --- true
+10<=5 --- false
+10>=5 --- true
+
+Que diferencia hay entre == y ===?
+- ==: Compara solo el valor
+Si los tipos son diferentes, JavaScript intenta convertirlos.
+5 == "5" -> true
+
+- === : compara el valor como el tipo:
+5 === "5" --> false
+*/
+
+// 4.- Operadores Logicos:
+/* Son operadores que trabajan con valores boleanos y deveulven un resultado logico
+
+*/
+
+
+
+
