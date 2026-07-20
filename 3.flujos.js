835cd41d1661271b37883df229df2c0dae96c7f4
Carlos Marcos <carlos@hotmail.com>
2026-07-20 15:19:03 -0500

    feat: se implemento la estructura de control de flujo

--- 3.flujos.js
+++ 3.flujos.js
@@ -1,1 +1,18 @@
 // Estructura de control de Flujo en Js
+
+/*Condicionales:
+Las condicionales permiten ejecutar un bloque de codigo dependiendo si se cumple
+una condicion o no.
+
+1. Condicionales Simples
+Estructura:
+if (condicion) {
+    // codigo a ejecutar si la condicion es verdadera
+}
+
+Ejemplo:
+if (edad >= 18) {
+    console.log("Eres mayor de edad");
+}
+    
+*/
