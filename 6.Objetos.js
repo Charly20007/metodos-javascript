47f96fc68e983e6c47dbdf17ff856de03589a782
Carlos Marcos <carlos@hotmail.com>
2026-08-17 21:55:20 -0500

    dw

--- 6.Objetos.js
+++ 6.Objetos.js
@@ -5,6 +5,7 @@ Array = "¿Qué elementos tengo en esta lista?", por posiciones
 
 a)Objetos en Javascript:
 ------------------------
+Sirve para agrupar infomracion relacionada
 Colecciones de propiedades con nombre y valor, acceso a propiedades usando punto y corchete
 cada valor esta asociado a una clave 
 let persona = {
@@ -15,6 +16,9 @@ let persona = {
   }
 };
 
+Dentro del objeto tenemos pares
+propiedad: valor
+
 console.log(persona.nombre); 
 persona.saludar();           
 
@@ -24,6 +28,33 @@ persona.direccion = "Calle Falsa 123";  Agregar propiedad
 persona.edad = 31;                      Modificar propiedad
 delete persona.direccion;               Eliminar propiedad
 
+Los objeto pueden guardar cualquier valor
+incluso puede guardar otro objeto
+const persona = {
+  nombre: "Carlos",
+
+  direccion: {
+    ciudad: "Lima",
+    distrito: "Miraflores"
+  }
+};
+
+un objeto tambien puede guardar una funcion
+const persona = {
+  nombre: "Carlos",
+
+  saludar: function () {
+    console.log("Hola");
+  }
+};
+
+8. Otra forma de acceder a propiedades: []
+const persona = {
+  nombre: "Carlos",
+  edad: 25
+};
+
+console.log(persona["nombre"]);
 
 --------------------------------------------------------
 b) Arrays en Javascript:
@@ -35,6 +66,26 @@ Cada elemento tiene una posicion llamada indice
 Índice       0          1          2
              ↓          ↓          ↓
 frutas = ["Manzana",  "Pera",  "Naranja"]
+
+frutas
+
+índice       valor
+
+  0       "manzana"
+  1       "pera"
+  2       "naranja"
+
+Modificar un array
+const frutas = ["Manzana", "Pera", "Naranja"];
+
+// Modificar Pera por Mango
+frutas[1] = "Mango";
+
+// Agregar Uva al final
+frutas.push("Uva");
+
+// Eliminar Naranja
+frutas.pop();
 
 accedes mediante el indice:
 frutas[0] // "Manzana"
